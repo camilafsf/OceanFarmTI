a964b6446837490fd4741987ea31bdabfffac265
JuanHJ319 <juanhenrique.jeronimosilva@gmail.com>
2023-04-10 00:17:27 -0300

    muita coisa

---
+++ Assets/Script/UI/PauseMenu.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private Button pauseButton;
+
+    private bool isPaused = false;
+
+    private void Start()
+    {
+        // Esconder o menu de pausa no início
+        pauseMenuUI.SetActive(false);
+
+        // Adicionar um listener ao botão de "pausa/despausa"
+        pauseButton.onClick.AddListener(TogglePause);
+    }
+
+    private void TogglePause()
+    {
+        if (isPaused)
+        {
+            // Despausar o tempo do jogo
+            Time.timeScale = 1f;
+
+            // Esconder o menu de pausa
+            pauseMenuUI.SetActive(false);
+
+            // Definir a flag de pausa como falsa
+            isPaused = false;
+        }
+        else
+        {
+            // Pausar o tempo do jogo
+            Time.timeScale = 0f;
+
+            // Mostrar o menu de pausa
+            pauseMenuUI.SetActive(true);
+
+            // Definir a flag de pausa como verdadeira
+            isPaused = true;
+        }
+    }
+}
